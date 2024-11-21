633bc5c43a705887331176fb9a050777c89a5373
Ben Joseph <ben.joseph81@gmail.com>
2024-11-21 13:06:13 -0600

    Day 2 Part 2

--- Day 2/main.py
+++ Day 2/main.py
@@ -16,9 +16,23 @@ for match in lines:
             new_list.append(3)
     master_list.append(new_list)
 
+me = 0
+
 for match in master_list:
     elf = match[0]
-    me = match[1]
+    result = match[1]
+    if result == 2:
+      me = elf
+    if result == 1:
+        if elf != 1:
+            me = elf - 1
+        elif elf == 1:
+            me = 3
+    if result == 3:
+        if elf != 3:
+            me = elf + 1
+        elif elf == 3:
+            me = 1
     if elf == 3 and me == 1:
         score += me + 6
     elif elf == 1 and me == 3:
