af8cde4f4ea7b6db2e3702362e06e29250fc8cfc
Ben Joseph <ben.joseph81@gmail.com>
2024-11-22 16:33:41 -0600

    Day 3b

--- Day 3/main.py
+++ Day 3/main.py
@@ -6,27 +6,39 @@ master_list = []
 alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 
 
-def split_string(string):
-    midpoint = len(string) // 2
-    first_half = string[:midpoint]
-    second_half = string[midpoint:]
+# def split_string(string):
+#     midpoint = len(string) // 2
+#     first_half = string[:midpoint]
+#     second_half = string[midpoint:]
+#
+#     return first_half, second_half
 
-    return first_half, second_half
 
+while len(lines) > 0:
+    count = 0
+    test_list = []
+    master_list = []
 
-for item in lines:
-    part_1, part_2 = split_string(item)
-    for char in part_1:
-        if char in part_2:
+    while count < 3:
+        count += 1
+        test_list.append(lines[0])
+        lines.pop(0)
+
+    print(test_list)
+
+    for char in test_list[0]:
+        if char in test_list[1] and char in test_list[2]:
             got_it = char
     master_list.append(got_it)
 
-for item in master_list:
-    index = alphabet.index(item.lower())
-    index += 1
-    if item.islower():
-        score += index
-    elif item.isupper():
-        score += (index + 26)
+    print(master_list)
+
+    for item in master_list:
+        index = alphabet.index(item.lower())
+        index += 1
+        if item.islower():
+            score += index
+        elif item.isupper():
+            score += (index + 26)
 
 print(score)
